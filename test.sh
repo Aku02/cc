33cd2962b0d9f35e556d35184013ffaad1c210ef
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-04 22:12:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=00:05:00
 #SBATCH --account=def-gigor
 #SBATCH --cpus-per-task=1             # CPU cores/threads
-#SBATCH --gres=gpu:p100:1                # Number of GPUs (per node)
+#SBATCH --gres=gpu:t4:1                # Number of GPUs (per node)
 #SBATCH --output=./output/log/%x-%j.out   # STDOUT
 #SBATCH --array=1-1%1   # 4 is the number of jobs in the chain
 nvidia-smi
