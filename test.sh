c45ba027cbb27193ca2f3488e8e23a5b8616f9f7
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-05 20:31:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #SBATCH --account=def-gigor
 #SBATCH --cpus-per-task=1             # CPU cores/threads
 #SBATCH --gres=gpu:t4:1                # Number of GPUs (per node)
-#SBATCH --output=./output/log/%x-%j.out   # STDOUT
+#SBATCH --output=./output/%j.out   # STDOUT
 #SBATCH --array=1-1%1   # 4 is the number of jobs in the chain
 singularity build conda.sif Singularity.def
