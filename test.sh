941ac49de73f27fdfef5a75d3988f16aec04d11f
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-05 20:27:48 +0530

    blah

--- test.sh
+++ test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:05:00
+#SBATCH --time=03:00:00
 #SBATCH --account=def-gigor
 #SBATCH --cpus-per-task=1             # CPU cores/threads
 #SBATCH --gres=gpu:t4:1                # Number of GPUs (per node)
 #SBATCH --output=./output/log/%x-%j.out   # STDOUT
 #SBATCH --array=1-1%1   # 4 is the number of jobs in the chain
-nvidia-smi
-pwd
-python dummy.py
+singularity build /my_images/fmriprep-20.1.0.simg docker://poldracklab/fmriprep:20.1.0
