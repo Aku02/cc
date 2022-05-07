586cccfdb7ff142ab66bb2171954cf5aa7535909
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-08 01:52:24 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@
 #SBATCH --gres=gpu:t4:1                # Number of GPUs (per node)
 #SBATCH --output=./output/%j.out   # STDOUT
 #SBATCH --array=1-1%1   # 4 is the number of jobs in the chain
-singularity build conda.sif Singularity
+singularity build --fakeroot conda.sif gpu.def
