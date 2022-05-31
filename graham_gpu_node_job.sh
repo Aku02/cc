08952a46982820e02c8b535a4bdad9a0dab43b12
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-31 20:06:12 +0530

    Update graham_gpu_node_job.sh

--- graham_gpu_node_job.sh
+++ graham_gpu_node_job.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:2
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=127000M
-#SBATCH --time=3:00
+#SBATCH --time=00:05:00
+#SBATCH --job-name=test-env
 #SBATCH --account=def-gigor
-nvidia-smi
+#SBATCH --cpus-per-task=1             # CPU cores/threads
+#SBATCH --gres=gpu:p100:1                # Number of GPUs (per node)
+#SBATCH --mem=1000M                   # memory per node
+#SBATCH --output=./%x-%j.out
+#SBATCH --array=1-1%1   # 1 is the number of jobs in the chain
+
+# Load Singularity
+module load StdEnv/2020
+module load singularity
+
+# nvidia-smi
+
+SING_IMG=/home/akash02/projects/def-gigor/singularity/bonmo.sif
+
+singularity run --nv $SING_IMG
+
+source activate banmo-cu113
+conda list
