af90b727e1305bfa0b616487309c3c8f4836fa43
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-05-31 21:23:31 +0530

    Update graham_gpu_node_job.sh

--- graham_gpu_node_job.sh
+++ graham_gpu_node_job.sh
@@ -19,5 +19,5 @@ SING_IMG=/home/akash02/projects/def-gigor/singularity/banmo.sif
 singularity run --nv $SING_IMG
 
 echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
-echo "conda activate banmo-cu113" >> ~/.bashrc
+export /opt/conda/bin>:$PATH
 conda list
