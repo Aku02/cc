b92c1f3373d0f36494ddd5aa431663355e9940b2
Aku02 <64954672+Aku02@users.noreply.github.com>
2022-06-04 12:06:49 +0530

    Update graham_gpu_node_job.sh

--- graham_gpu_node_job.sh
+++ graham_gpu_node_job.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:2
-#SBATCH --ntasks-per-node=32
-#SBATCH --mem=127000M
-#SBATCH --time=3:00
+#SBATCH --time=00:05:00
+#SBATCH --job-name=test
 #SBATCH --account=def-gigor
+#SBATCH --cpus-per-task=1            
+#SBATCH --gres=gpu:t4:1               
+#SBATCH --mem=1000M                   
+#SBATCH --output=./%x-%j.out
+#SBATCH --mail-user=akash.karthikeyan@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-1%1   # 1 is the number of jobs in the chain
 nvidia-smi
+source activate banmo-cu113
+
+which python
+python script.py
